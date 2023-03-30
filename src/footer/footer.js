ee2979adc61a42ca74641590f1767b07c380e4d6
oyewunmi10 <oyewunmiolaniyi72@gmail.com>
2023-03-30 14:56:55 +0100

    footer

---
+++ src/footer/footer.js
@@ -0,0 +1,47 @@
+import React from 'react' ;
+import './footer.css' ;
+
+
+const footer = () => {
+  return (
+    <div className="frontRow"  >
+        <div className="small"  >
+            <h1 className="gradients_text">do you want to step in to the future </h1>
+        </div>
+        <div className='footer.btn'>
+            <p>Requests Early Access </p>
+        </div>
+        <div  className='left-side'> 
+            <h4>links</h4>
+            <p>Contact us </p>
+            <p>Social media</p>
+            <p>contacts</p>
+            <p>counters</p>
+            <p>...</p>
+        </div>        
+        <div  className='left-side'> 
+            <h4>Company</h4>
+            <p>Terms & conditions  </p>
+            <p>Social media</p>
+            <p>Privacy policy </p>
+            <p>contacts</p>
+            <p>...</p>
+        </div>      
+        <div  className='left-side'> 
+            <h4>get in touch</h4>
+            <p>456b Psalm Street Alagomeji, Yaba Lagos-State </p>
+            <p>09013700452</p>
+            <p>mariaMustapha@gmail.com</p>
+            <p>...</p>
+        </div>      
+        <div className='ending'>
+            <p>2023...  All Right Reserved .</p>
+        </div>
+        
+    </div>    
+        )
+}     
+
+
+ 
+export default footer
